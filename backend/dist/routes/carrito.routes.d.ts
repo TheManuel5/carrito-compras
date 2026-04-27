1370f338e2dc884c1cf996c5c3e28df8f4de65c2
Manuel Arteaga <videosthemanuel@gmail.com>
2026-04-27 17:05:25 -0500

    clean gitignore fix

--- backend/dist/routes/carrito.routes.d.ts
+++
@@ -1,3 +0,0 @@
-declare const router: import("express-serve-static-core").Router;
-export default router;
-//# sourceMappingURL=carrito.routes.d.ts.map
--- backend/dist/routes/producto.routes.d.ts
+++
@@ -1,3 +0,0 @@
-declare const router: import("express-serve-static-core").Router;
-export default router;
-//# sourceMappingURL=producto.routes.d.ts.map
